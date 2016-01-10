a1f3f8e373d891b747c9b962cf361e5206a3920f
Cody <codylmathisen@gmail.com>
2016-01-10 01:27:57 -0800

    added ban/selected styles for maps

---
+++ app/js/app.js
@@ -0,0 +1,53 @@
+'use strict'
+
+var app = angular.module('evolvePickerApp',
+    ['ngRoute']
+).run();
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        controller: 'MasterController',
+        templateUrl: '/partials/maps.html',
+    });
+    $locationProvider.html5Mode(true);
+});
+
+app.controller('MasterController', function($scope, MAPS) {
+    console.log('master');
+
+    $scope.maps = [];
+    angular.forEach(MAPS, function(map) {
+        $scope.maps.push({
+            name: map,
+            image: '/img/map/' + map.replace(' ', '_') + '.jpg'
+        });
+    });
+
+
+});
+
+app.service('BanManager', function() {
+    var maps_ban = [undefined, undefined];
+    var maps_select = [undefined, undefined, undefined];
+
+    this.addMapBan = function(team, map) {
+        maps_banned[team] = map;
+    }
+
+    this.addMapSelect = function(round, map) {
+        maps_select[round] = map;
+    }
+});
+
+app.constant('MAPS', [
+    'armory',
+    'barracks',
+    'distillery',
+    'fusion plant',
+    'medlab',
+    'orbital drill',
+    'refueling tower',
+    'rendering plant',
+    'weather control',
+    'wraith trap'
+]);
